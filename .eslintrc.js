948642992dd6d8434db996fd3d6b74c5e63cf8ff
Salomão Neto <salomaosnff3+github@gmail.com>
2023-08-09 17:00:22 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021' : true,
+  },
+  'extends'  : './lib/index.js',
+  'overrides': [
+    {
+      'env'          : { 'node': true },
+      'files'        : ['.eslintrc.{js,cjs}'],
+      'parserOptions': { 'sourceType': 'script' },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType' : 'module',
+  },
+  'rules': {},
+};
